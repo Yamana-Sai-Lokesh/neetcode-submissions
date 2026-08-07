13a8285e5bbba189d38b6a2db42add6c140feb60
YAMANA SAI LOKESH <115028122+Yamana-Sai-Lokesh@users.noreply.github.com>
2026-08-07 19:36:21 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        int n = nums.size();
+        for(int i =0;i<n;i++)
+        {
+            mp[nums[i]]=i;
+        }
+        for(int i = 0;i<n;i++){
+            int diff = target-nums[i];
+            if(mp.find(diff)!=mp.end() && mp[diff]!=i){
+                return {min(i, mp[diff]), max(i, mp[diff])};
+            }
+        }
+        return {};
+    }
+};
